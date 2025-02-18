198edf64ed6173ca320376c28680e125d3e8d0a8
JT-Higgins <jackthomas.jt.higgins@gmail.com>
2025-02-18 16:08:35 -0700

    initial swift commit

---
+++ Closet Companion/Closet_CompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Closet_CompanionApp.swift
+//  Closet Companion
+//
+//  Created by JT Higgins on 2/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Closet_CompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Closet CompanionTests/Closet_CompanionTests.swift
@@ -0,0 +1,17 @@
+//
+//  Closet_CompanionTests.swift
+//  Closet CompanionTests
+//
+//  Created by JT Higgins on 2/18/25.
+//
+
+import Testing
+@testable import Closet_Companion
+
+struct Closet_CompanionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
